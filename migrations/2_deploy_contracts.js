cef5fdb6b823a5aa50fb53b348d54335b942fcd0
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-10-26 13:16:06 +0700

    Change airdrop

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,6 +14,10 @@ module.exports = function(deployer) {
         const ad = await Airdrop.at('0xe6372344627fC54BBA8F96F89fa3F0e6bc10fBeC');
         const ct = await CrossToken.at('0x014be200c192bD8366dF781a96cec51B3D9Dcd93');
 
+        // Testnet
+        // const ad = await Airdrop.at('0xda323dfaaa23fcb751bd1a4bc4be83bd91d7fce7');
+        // const ct = await CrossToken.at('0x920A80E7DA9d2895A1AED17bFC8Db1B310a613DE');
+
         // const totalSupply = ether('55000000');
         // await ct.transfer(ad.address, totalSupply);
 
--- src/getHolders.js
+++ src/getHolders.js
@@ -1,66 +1,36 @@
 const axios = require('axios');
-const HTML = require('node-html-parser');
 const fs = require('fs');
 
-const address = '0x360673a34cf5055dfc22c53bc063e948a243293b';
+axios.post('https://graphql.bitquery.io/', {
+    "query":"query ($network: EthereumNetwork!, $token: String!, $zero: String!, $limit: Int!, $offset: Int!, $from: ISO8601DateTime) {\n  ethereum(network: $network) {\n    transfers(\n      options: {desc: \"block.timestamp.time\", limit: $limit, offset: $offset}\n      date: {since: $from}\n      amount: {gt: 0}\n      currency: {is: $token}\n      sender: {is: $zero}\n    ) {\n      block(time: {before: \"2021-10-23T19:28:04.000Z\"}) {\n        timestamp {\n          time(format: \"%Y-%m-%d %H:%M:%S\")\n        }\n      }\n      sender {\n        address\n      }\n      receiver {\n        address\n      }\n      currency {\n        symbol\n      }\n      count\n      transaction {\n        hash\n      }\n    }\n  }\n}\n",
+    "variables":"{\"limit\":1000,\"offset\":0,\"network\":\"bsc\",\"token\":\"0x360673a34cf5055dfc22c53bc063e948a243293b\",\"zero\":\"0x0000000000000000000000000000000000000000\",\"from\":\"2021-09-06\",\"dateFormat\":\"%Y-%m-%d\"}"
+}).then(response => {
+    let data = response.data.data.ethereum.transfers;
+    let res = [];
+    let temp = {};
+    let total = 0;
+    for(let i = 0; i < data.length; i++) {
+        if (temp[data[i]['receiver']['address']] == undefined) {
+            temp[data[i]['receiver']['address']] = 0
+        }
+        temp[data[i]['receiver']['address']] += data[i]['count']
+        total += data[i]['count']
+    }
 
-let total_supply = 0;
+    console.log(total);
 
-let res = [];
-let resTS = 0;
-
-async function main() {
-    await axios.get('https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x360673a34cf5055dfc22c53bc063e948a243293b&apikey=YourApiKeyToken')
-    .then(function (response) {
-        total_supply = parseInt(response.data.result);
-        //console.log(response.data.result);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-    let p = 1;
-    let whileTrue = true;
-    while (whileTrue) {
-        await axios.get('https://bscscan.com/token/generic-tokenholders2?a=' + address + '&sid=&m=normal&p=' + p)
-        .then(function (response) {
-            let html = response.data;
-
-            let trs = HTML.parse(html).querySelector('tbody').querySelectorAll('tr');
-
-            trs.forEach(element => {
-                //console.log(element.querySelector('a').text); 
-                //console.log(element.querySelectorAll('td')[2].text);
-
-                resTS += parseInt(element.querySelectorAll('td')[2].text);
-                
-                if (element.querySelector('a').text != '0x36894d06ac91b09760b4310c75ed2348e3ea063c') {
-                    res.push({
-                        'address': element.querySelector('a').text,
-                        'value': element.querySelectorAll('td')[2].text
-                    });
-                }
-            });
-
-            if (trs.length == 50) {
-                p++;
-            } else {
-                whileTrue = false;
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
+    for (const [key, value] of Object.entries(temp)) {
+        res.push({
+            'address': key,
+            'value': value
         });
     }
 
-    if (resTS != total_supply ) {
-        console.log("ERROR!");
-    }
-    
     fs.writeFile("holders.json", JSON.stringify(res), function(err) {
         if (err) {
             console.log(err);
         }
     });
-}
-
-main();
+}).catch(error => {
+    console.log(error);
+});
